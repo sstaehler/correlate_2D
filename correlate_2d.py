26dcfa38c7f9ab2393a57ed4c390681b2779e28e
sstaehler <staehler@geophysik.uni-muenchen.de>
2015-11-24 10:09:42 +0100

    Working multiprocessing with argument parser
     - allows to select the variable to correlate
     - also a maximum correlation length. Elements pairs with a larger
       distance in between are masked
     - progress bar works as well

--- correlate_2d.py
+++ correlate_2d.py
@@ -2,16 +2,13 @@
 # coding: utf-8
 
 import netCDF4
-# from progressbar import ProgressBar, ETA, Bar, RotatingMarker, Percentage
+from progressbar import ProgressBar, ETA, Bar, Percentage
 import numpy as np
 import scipy.stats
+import time
+import argparse
 from multiprocessing import Pool
 # import os
-
-filename = 'BSWave2006.nc'
-file_in = netCDF4.Dataset(filename)
-
-variable = 'tmm10'
 
 
 def calc_corr(iel):
@@ -24,21 +21,52 @@ def calc_corr(iel):
 
     if not np.ma.is_masked(data_2d[:, lat_idx, lon_idx]):
 
-        ts_here = data_2d[:, lat_idx, lon_idx]
-
-        conv = np.sum(ts_here[:, None, None] * data_2d[:, :, :], axis=0)
-        conv *= normvec * normvec[lat_idx, lon_idx]
-
-        convs = conv.flatten()
         distances = np.sqrt(np.cos(lat)**2 * (lat-latlat)**2 +
                             (lon-lonlon)**2).flatten() * 111.
 
+        ts_here = data_2d[:, lat_idx, lon_idx]
+
+        if dist_max>0:
+            data_masked = np.ma.masked_where(distances>dist_max, data_2d[:,:,:])
+        else:
+            data_masked = data_2d[:,:,:]
+
+        conv = np.sum(ts_here[:, None, None] * data_masked, axis=0)
+        conv *= normvec * normvec[lat_idx, lon_idx]
+
+        convs = conv.flatten()
         slope, intercept, a, b, c = scipy.stats.linregress(distances,
                                                            np.log(convs))
     return slope  # , intercept
 
 if __name__ == '__main__':
+
+    helptext = 'Calculate spatial correlation length in a 2D time series'
+    formatter_class = argparse.RawTextHelpFormatter
+    parser = argparse.ArgumentParser(description=helptext,
+                                     formatter_class=formatter_class)
+
+    helptext = 'NetCDF file name. Should follow BLABLA convention'
+    parser.add_argument('file_name', help=helptext)
+
+    helptext = 'Variable to calculate correlation on'
+    parser.add_argument('-v', '--variable', help=helptext)
+
+    helptext = 'Maximum correlation length. \n' + \
+               'Only elements with a distance smaller max_length are \n' + \
+               'correlated. Smaller max_length speed up the calculation'
+    parser.add_argument('-m', '--max_length', help=helptext, default=-1)
+
+    args = parser.parse_args()
+
+    filename = args.file_name
+    file_in = netCDF4.Dataset(filename)
+
+    variable = args.variable
     data_2d = file_in.variables[variable]
+
+    dist_max = args.max_length
+
     lats = file_in.variables['latitude']
     lons = file_in.variables['longitude']
 
@@ -51,10 +79,20 @@ if __name__ == '__main__':
 
     slopes = np.zeros(nelements)
     intercepts = np.zeros(nelements)
-    # widgets = ['Correlating: ', Percentage(), ' ',
-    #             Bar(marker=RotatingMarker()),
-    #         ' ', ETA()]
-    # pbar = ProgressBar(widgets = widgets, maxval=nelements).start()
+    widgets = ['Correlating: ', Percentage(), ' ',
+               Bar(), ' ', ETA()]
+    pbar = ProgressBar(widgets=widgets, maxval=nelements).start()
 
     p = Pool(processes=2)
-    slopes = p.map(calc_corr, range(0, nelements))
+    result = p.map_async(calc_corr, range(0, nelements), chunksize=1)
+
+    while not result.ready():
+        ndone = nelements - result._number_left
+        pbar.update(ndone)
+        time.sleep(1)
+        if ndone == nelements:
+            break
+    p.close()
+    p.join()
+
+    slopes = result.get()
