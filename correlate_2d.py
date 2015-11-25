1d446daa3bc2d5877b05e69da81147c27e99afb2
sstaehler <staehler@geophysik.uni-muenchen.de>
2015-11-25 15:21:52 +0100

    Working version with bells and whistles
     - saves result to NetCDF file of similar shape
     - speed up due to parameter maximum_distance (only calculates
       correlation coefficient up to this distance)
     - Tested against manual version

--- correlate_2d.py
+++ correlate_2d.py
@@ -4,50 +4,24 @@
 import netCDF4
 from progressbar import ProgressBar, ETA, Bar, Percentage
 import numpy as np
-import scipy.stats
 import time
 import argparse
+# import matplotlib.pyplot as plt
 from multiprocessing import Pool
 # import os
 
 
-def calc_corr(iel):
-    slope = 0
-    intercept = -1
-    lat = latlat.ravel()[iel]
-    lon = lonlon.ravel()[iel]
-    lat_idx = np.abs(lats[:] - lat).argmin()
-    lon_idx = np.abs(lons[:] - lon).argmin()
-
-    if not np.ma.is_masked(data_2d[:, lat_idx, lon_idx]):
-
-        distances = np.sqrt(np.cos(lat)**2 * (lat-latlat)**2 +
-                            (lon-lonlon)**2).flatten() * 111.
-
-        ts_here = data_2d[:, lat_idx, lon_idx]
-
-        if dist_max>0:
-            data_masked = np.ma.masked_where(distances>dist_max, data_2d[:,:,:])
-        else:
-            data_masked = data_2d[:,:,:]
-
-        conv = np.sum(ts_here[:, None, None] * data_masked, axis=0)
-        conv *= normvec * normvec[lat_idx, lon_idx]
-
-        convs = conv.flatten()
-        slope, intercept, a, b, c = scipy.stats.linregress(distances,
-                                                           np.log(convs))
-    return slope  # , intercept
-
-if __name__ == '__main__':
-
+def define_argument_parser():
     helptext = 'Calculate spatial correlation length in a 2D time series'
     formatter_class = argparse.RawTextHelpFormatter
     parser = argparse.ArgumentParser(description=helptext,
                                      formatter_class=formatter_class)
 
-    helptext = 'NetCDF file name. Should follow BLABLA convention'
-    parser.add_argument('file_name', help=helptext)
+    helptext = 'NetCDF input file name. Should follow BLABLA convention'
+    parser.add_argument('input_file_name', help=helptext)
+
+    helptext = 'Output file name. Default is input_file_name_corr.nc'
+    parser.add_argument('-o', '--output_file_name', help=helptext)
 
     helptext = 'Variable to calculate correlation on'
     parser.add_argument('-v', '--variable', help=helptext)
@@ -57,42 +31,161 @@ if __name__ == '__main__':
                'correlated. Smaller max_length speed up the calculation'
     parser.add_argument('-m', '--max_length', help=helptext, default=-1)
 
-    args = parser.parse_args()
+    helptext = 'Depth layer to correlate. '
+    parser.add_argument('-d', '--depth_layer', help=helptext, default=0)
 
-    filename = args.file_name
+    args = parser.parse_args()
+    return args
+
+
+def distance(lat, lon, latgrid, longrid):
+    # Since this is a small region, this approximation is a precise enough
+    # and a lot faster than calculating the true Great Circle Arc distance
+    R = 6371  # radius of the earth in km
+    x = (longrid - lon) * np.cos(0.5*(latgrid+lat))
+    y = latgrid - lat
+    d = R * np.sqrt(x*x + y*y)
+    return d
+
+
+def conv_small_dist(A, idx_x, idx_y, dx_max, dy_max):
+
+    x_min = np.max((idx_x - dx_max, 0))
+    x_max = np.min((idx_x + dx_max + 1, A.shape[0] + 1))
+    y_min = np.max((idx_y - dy_max, 0))
+    y_max = np.min((idx_y + dy_max + 1, A.shape[1] + 1))
+
+    ts_here = A[:, idx_x, idx_y]
+    conv = np.sum(A[:, x_min:x_max, y_min:y_max] *
+                  ts_here[:, None, None], axis=0)
+
+    conv *= normvec[x_min:x_max, y_min:y_max] * normvec[idx_x, idx_y]
+
+    distances = distance(latlat[idx_x, idx_y], lonlon[idx_x, idx_y],
+                         latlat[x_min:x_max, y_min:y_max],
+                         lonlon[x_min:x_max, y_min:y_max]).flatten()
+
+    # plt.pcolormesh(normvec[x_min:x_max, y_min:y_max])
+    # p = plt.pcolormesh((A[0, x_min:x_max, y_min:y_max]))
+    # plt.colorbar(p)
+    # plt.plot(distances, conv.flatten(), '+')
+    # plt.show()
+    return conv.flatten()[distances > 0], distances[distances > 0]
+
+
+def calc_corr(iel):
+    lat = latlat.ravel()[iel]
+    lon = lonlon.ravel()[iel]
+    lat_idx = np.abs(lats[:] - lat).argmin()
+    lon_idx = np.abs(lons[:] - lon).argmin()
+
+    # Check, whether we are outside of the model domain
+    if np.mean(abs(data_2d_demean[:, lat_idx, lon_idx])) < 1e-4:
+        corr_len = 0.0
+    else:
+        convs, distances = conv_small_dist(data_2d_demean,
+                                           lat_idx, lon_idx,
+                                           dxi, dyi)
+        corr_len = 1. / \
+            np.mean((-np.log(convs)/distances)[distances <= dist_max])
+    return corr_len
+
+
+if __name__ == '__main__':
+
+    args = define_argument_parser()
+
+    # Open NetCDF input file
+    filename = args.input_file_name
     file_in = netCDF4.Dataset(filename)
 
-    variable = args.variable
-    data_2d = file_in.variables[variable]
+    # Get name of NetCDF output file
+    if not args.output_file_name:
+        filename_split = filename.split('.')
+        file_out = ''.join(filename_split[0:-1]) + '_corr.' + filename_split[-1]
+    else:
+        file_out = args.output_file_name
 
-    dist_max = args.max_length
+    # Load variable to correlate
+    variable_name = args.variable
+    data_2d = file_in.variables[variable_name]
 
-    lats = file_in.variables['latitude']
-    lons = file_in.variables['longitude']
+    # Get maximum distance to correlate
+    dist_max = float(args.max_length)
+    print 'Maximum distance to correlate: %d km' % dist_max
 
-    latlat, lonlon = np.meshgrid(lats[:], lons[:])
+    # Get depth layer on which to correlate
+    idepth = args.depth_layer
+
+    # Read latitude and longitude vectors and create mesh grid
+    lats = file_in.variables['latc'][:] * np.pi / 180.
+    lons = file_in.variables['lonc'][:] * np.pi / 180.
+    lonlon, latlat = np.meshgrid(lons, lats)
+
+    # Calculate maximum distance to calculate CC on. Separately for X and Y.
+    dx = np.mean(np.diff(lons)) * 180 / np.pi * np.cos(np.mean(lats)) * 111.
+    dy = np.mean(np.diff(lats)) * 180 / np.pi * 111.
+    print 'Grid spacing: %8.2f/%8.2f km' % (dx, dy)
+    dxi = int(dist_max / dx)
+    dyi = int(dist_max / dy)
+    print 'Elements to evaluate: %d/%d' % (dxi, dyi)
+
+    # Get number of grid points
     nelements = np.prod(latlat.shape)
+    print 'Calculating correlation on %d elements' % nelements
 
-    data_2d.set_auto_mask(True)
+    # Remove mean from data (correlation coefficient requires mean==0)
+    data_2d_demean = data_2d[:, idepth, :, :] \
+        - np.mean(data_2d[:, idepth, :, :], axis=0)
 
-    normvec = 1. / np.sqrt(np.sum((data_2d[:, :, :])**2, axis=0))
+    # Calculate normalization matrix
+    normvec = np.sqrt(np.sum((data_2d_demean)**2, axis=0))
+    normvec[normvec > 1e-10] = 1./normvec[normvec > 1e-10]
 
-    slopes = np.zeros(nelements)
-    intercepts = np.zeros(nelements)
+    # Define progress bar
     widgets = ['Correlating: ', Percentage(), ' ',
                Bar(), ' ', ETA()]
     pbar = ProgressBar(widgets=widgets, maxval=nelements).start()
 
-    p = Pool(processes=2)
+    # Define pool of parallel workers
+    p = Pool()
     result = p.map_async(calc_corr, range(0, nelements), chunksize=1)
+    p.close()
 
+    # Calculate all elements on the workers until all done
     while not result.ready():
         ndone = nelements - result._number_left
         pbar.update(ndone)
         time.sleep(1)
-        if ndone == nelements:
-            break
-    p.close()
     p.join()
 
-    slopes = result.get()
+    print '...done'
+
+    # Save result into 2D variable
+    corrlen = np.array(result.get()).reshape(latlat.shape)
+    # pc = plt.pcolormesh(slopes)
+    # plt.colorbar(pc)
+    # plt.show()
+
+    # Save correlated variable into new netCDF file
+
+    with netCDF4.Dataset(file_out, mode='w') as dst:
+
+        for name, dimension in file_in.dimensions.iteritems():
+            if not dimension.isunlimited():
+                dst.createDimension(name, len(dimension))
+            else:
+                dst.createDimension(name, None)
+
+        for name, variable in file_in.variables.iteritems():
+            # do not copy the variable of choice
+            if name == variable_name:
+                x = dst.createVariable(name, variable.datatype,
+                                       variable.dimensions[2:])
+                dst.variables[name][:, :] = corrlen
+            else:
+                x = dst.createVariable(name, variable.datatype,
+                                       variable.dimensions)
+                dst.variables[name][:] = file_in.variables[name][:]
+
+        var_corrlen = dst.createVariable
